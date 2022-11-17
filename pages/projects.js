783a4241b34ce0d950019089ce021ca1335accb4
fireballer150 <cutterballer@gmail.com>
2022-11-17 10:44:35 +0900

    배포준비완료

--- pages/projects.js
+++ pages/projects.js
@@ -3,71 +3,67 @@ import Head from "next/head";
 import { TOKEN, DATABASE_ID } from "../config";
 import ProjectItem from "../components/projects/project-item";
 
-export default function Projects({projects}) {
+export default function Projects({ projects }) {
+  console.log(projects);
 
-    console.log(projects);
+  return (
+    <Layout>
+      <div className="flex flex-col items-center justify-center min-h-screen px-3 mb-10">
+        <Head>
+          <title>이희수 포트폴리오</title>
+          <meta name="description" content="오늘도 빡코딩!" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <h1 className="text-4xl font-bold sm:text-6xl">
+          총 프로젝트 :
+          <span className="pl-4 text-blue-500">{projects.results.length}</span>
+        </h1>
 
-    return (
-        <Layout >
-            <div className="flex flex-col items-center justify-center min-h-screen px-3 mb-10">
-                <Head>
-                    <title>빡코딩 포트폴리오</title>
-                    <meta name="description" content="오늘도 빡코딩!" />
-                    <link rel="icon" href="/favicon.ico" />
-                </Head>
-                <h1 className="text-4xl font-bold sm:text-6xl">
-                    총 프로젝트 :
-                    <span className="pl-4 text-blue-500">{projects.results.length}</span>
-                </h1>
-
-                <div className="grid grid-cols-1 gap-8 p-12 m-4 md:grid-cols-2">
-                    {projects.results.map((aProject) => (
-                        <ProjectItem key={aProject.id} data={aProject}/>
-                    ))}
-                </div>
-            </div>
-        </Layout>
-    );
+        <div className="grid grid-cols-1 gap-8 p-12 m-4 md:grid-cols-2">
+          {projects.results.map((aProject) => (
+            <ProjectItem key={aProject.id} data={aProject} />
+          ))}
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
-// 빌드 타임에 호출
-// export async function getStaticProps() {
-
-// 각 요청 때마다 호출
 export async function getServerSideProps() {
-
-    const options = {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Notion-Version': '2022-02-22',
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${TOKEN}`
+  const options = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Notion-Version": "2022-02-28",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${TOKEN}`,
+    },
+    body: JSON.stringify({
+      sorts: [
+        {
+          property: "Name",
+          direction: "ascending",
         },
-        body: JSON.stringify({
-            sorts: [
-                {
-                    "property": "Name",
-                    "direction": "ascending"
-                }
-            ],
-            page_size: 100
-        })
-      };
+      ],
+      page_size: 100,
+    }),
+  };
 
-    const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options)
+  const res = await fetch(
+    `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+    options
+  );
 
-    const projects = await res.json()
+  const projects = await res.json();
 
-    const projectNames = projects.results.map((aProject) =>(
-        aProject.properties.Name.title[0].plain_text
-    ))
+  const projectNames = projects.results.map(
+    (aProject) => aProject.properties.Name.title[0]?.plain_text
+  );
 
-    console.log(`projectNames : ${projectNames}`);
+  console.log(`projectNames : ${projectNames}`);
 
-    return {
-      props: {projects}, // will be passed to the page component as props
-      // getStaticProps() 메소드를 사용한다면 revalidate 로 데이터 변경시 갱신가능!
-      // revalidate: 1 // 데이터 변경이 있으면 갱신 1초 마다
-    }
+  return {
+    props: { projects },
+    revalidate: 1,
+  };
 }
